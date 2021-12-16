b5b1010da62017d91e4449898fbea8d79798282f
root <b-semen-b@mail.ru>
2021-12-16 16:18:30 +0300

    serializers

--- scanner/serializers.py
+++ scanner/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import TokenContract, ProbateContract, WeddingContract, CrowdsaleContract
+from .models import TokenContract, ProbateContract, WeddingContract, CrowdsaleContract, Profile
 
 
 class TokenSerializer(serializers.ModelSerializer):
@@ -37,3 +37,43 @@ class ProbateListSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProbateContract
         fields = ('address', 'mails_array', 'owner_mail')
+
+
+class ProbateCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProbateContract
+        fields = ('address', 'name', 'mails_array', 'owner', 'owner_mail')
+
+    def create(self, validated_data):
+        return ProbateContract.objects.update_or_create(**validated_data)
+
+
+class CrowdsaleCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CrowdsaleContract
+        fields = ('address', 'name', 'owner')
+
+    def create(self, validated_data):
+        return CrowdsaleContract.objects.update_or_create(**validated_data)
+
+
+class WeddingCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = WeddingContract
+        fields = ('address', 'name', 'mail_list', 'owner')
+
+    def create(self, validated_data):
+        return WeddingContract.objects.update_or_create(**validated_data)
+
+
+class TokenCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = TokenContract
+        fields = ('address', 'address_list', 'name', 'owner')
+
+    def create(self, validated_data):
+        return TokenContract.objects.update_or_create(**validated_data)
