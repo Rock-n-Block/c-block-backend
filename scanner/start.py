e3c145f6e7c96778501fbe0bbacb74433a913a28
root <b-semen-b@mail.ru>
2021-12-22 16:25:12 +0300

    add reconnect w3. add catch tx_hash

--- scanner/start.py
+++ scanner/start.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import asyncio
+from web3 import Web3
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mywish2.settings")
@@ -8,7 +9,7 @@ import django
 django.setup()
 
 from mywish2.settings import config
-from scanner.utils import get_event, create_token, create_probate
+from scanner.utils import get_event, create_token, create_probate, create_wedding
 import logging
 
 
@@ -19,19 +20,26 @@ if __name__ == "__main__":
         for network in [config.test_network, config.network]:
             logger = logging.getLogger(f'{network.name}_scanner')
             logger.info('Start scanner')
+            network.w3 = Web3(Web3.WebsocketProvider(network.ws_endpoint))
             # List contract addresses
-            token_contract = network.token_factories
-            probate_contract = network.probate_factories
+            token_contracts = network.token_factories
+            probate_contracts = network.probate_factories
+            wedding_contracts = network.wedding_factories
             provider_url = network.ws_endpoint
             # add all contracts event
             futures += [get_event(
                 address, provider_url,
                 network.test, network.w3, '(address,uint8)',
                 create_token, logger
-            ) for address in token_contract]
+            ) for address in token_contracts]
             futures += [get_event(
                 address, provider_url,
                 network.test, network.w3, '(address,uint256)',
                 create_probate, logger
-            ) for address in probate_contract]
+            ) for address in probate_contracts]
+            futures += [get_event(
+                address, provider_url,
+                network.test, network.w3, '(address,address)',
+                create_wedding, logger
+            ) for address in probate_contracts]
         loop.run_until_complete(asyncio.wait(futures))
--- scanner/utils.py
+++ scanner/utils.py
@@ -4,8 +4,9 @@ from typing import Callable
 from websockets import connect
 from eth_abi import decode_single
 
-from .models import TokenContract, Profile, ProbateContract
+from .models import TokenContract, Profile, ProbateContract, WeddingContract
 from mywish2.settings import config
+from scanner.contracts import PROBATE_FABRIC_ABI
 
 from django.core.mail import send_mail
 from asgiref.sync import sync_to_async
@@ -20,13 +21,23 @@ def create_token(decoded: list, profile: Profile, test: bool, logger):
         test_node=test)
 
 
-def create_probate(decoded: list, profile: Profile, test: bool, logger):
+def create_probate(decoded: list, profile: Profile, test: bool, logger, tx_hash) -> list:
     logger.info('Probate saved')
     return ProbateContract.objects.update_or_create(
         address=decoded[0],
         dead=False,
         owner=profile,
-        test_node=test)
+        test_node=test,
+        tx_hash=tx_hash)
+
+
+def create_wedding(decoded: list, profile: Profile, test: bool, logger, tx_hash) -> list:
+    logger.info('Probate saved')
+    return WeddingContract.objects.update_or_create(
+        address=decoded[0],
+        owner=profile,
+        test_node=test,
+        tx_hash=tx_hash)
 
 
 
@@ -50,6 +61,7 @@ async def get_event(contract: str, node: str, test: bool, w3, event: str, handle
                 "id": 1,
                 "method": "eth_subscribe",
                 "params": [
+
                     "logs", {
                         "address": [contract],
                         "topics": [w3.keccak(text=f"NewContract{event}").hex()]
@@ -63,32 +75,35 @@ async def get_event(contract: str, node: str, test: bool, w3, event: str, handle
 
                 # Decoded response with event
                 data = json.loads(message)['params']['result']['data'][2:]
+                tx_hash = json.loads(message)['params']['result']['transactionHash']
                 decoded = list(decode_single(f'{event}', bytearray.fromhex(data)))
                 logger.info(f'New Event {decoded}')
-                decoded.append('0xsssemyon') # Only for test
+                owner_address = w3.eth.get_transaction(tx_hash)['from']
+                logger.info(owner_address)
                 profile, create = await sync_to_async(Profile.objects.get_or_create, thread_sensitive=True)(
-                    owner_address__iexact=decoded[2])
+                    owner_address=owner_address)
                 logger.info(decoded)
-                await sync_to_async(handler, thread_sensitive=True)(decoded, profile, test, logger)
+                await sync_to_async(handler, thread_sensitive=True)(decoded, profile, test, logger,tx_hash)
                 logger.info('Done')
 
-            except:
+            except Exception as e:
                 # Celo disconnect sometimes and we need reconnect
                 if not w3.isConnected():
                     logger.warning('Connection reset')
-                    w3.WebSocketProvider(node)
+                    return w3.WebSocketProvider(node)
                 else:
                     pass
 
 
 def send_heirs_mail(owner_mail: str, heirs_mail_list: list) -> None:
-    [send_mail(
-        'Owner is dead',
-        'My apologise, but owner is fucking dead.\nSadness =-(',
-        from_email=config.email_host_user,
-        recipient_list=[mail],
-        fail_silently=True,
-    ) for mail in heirs_mail_list]
+    for mail in heirs_mail_list:
+        send_mail(
+            'Owner is dead',
+            'My apologise, but owner is fucking dead.\nSadness =-(',
+            from_email=config.email_host_user,
+            recipient_list=[mail],
+            fail_silently=True,
+        )
     send_mail(
         'You are dead',
         'My apologise, but owner is fucking dead.\nSadness =-(',
@@ -96,3 +111,15 @@ def send_heirs_mail(owner_mail: str, heirs_mail_list: list) -> None:
         recipient_list=[owner_mail],
         fail_silently=True,
     )
+
+
+def check_terminated_contract(test: bool, address: str):
+    """
+    Check that contract is not terminated and change status if terminated"""
+    if test:
+        w3 = config.test_network.w3
+    else:
+        w3 = config.network.w3
+    contract = w3.eth.contract(address=w3.toChecksumAddress(address), abi=PROBATE_FABRIC_ABI)
+    terminated = contract.functions.terminated().call()
+    ProbateContract.objects.update(terminated=terminated)
