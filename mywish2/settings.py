7acc4a8d20ec9e1db16ce643c6a8649f9daa8dbe
Euphemia Deathridge <ephdtrg@mintyclouds.in>
2022-02-25 14:47:25 +0300

    fix

--- mywish2/settings.py
+++ mywish2/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions'
+    'django_extensions',
     'drf_yasg',
     'rest_framework',
     'scanner',
