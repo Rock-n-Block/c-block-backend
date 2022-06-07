43d6660fce286259abc74a500bb8968dfba449e4
Euphemia Deathridge <ephdtrg@mintyclouds.in>
2022-06-07 16:37:03 +0300

    custom redirects on signup

--- cblock/accounts/adapters.py
+++ cblock/accounts/adapters.py
@@ -1,3 +1,5 @@
+import logging
+
 from urllib.parse import urlsplit
 
 from django.contrib.sites.shortcuts import get_current_site
@@ -7,17 +9,18 @@ from allauth.account.adapter import DefaultAccountAdapter
 from allauth.utils import build_absolute_uri
 
 from cblock.accounts.utils import get_domain_for_emails
-
+from cblock.settings import  config, ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL
 
 class CustomDomainAdapter(DefaultAccountAdapter):
 
-    def get_email_confirmation_url(self, request, emailconfirmation):
-        url = reverse("account_confirm_email", args=[emailconfirmation.key])
-        ret = build_absolute_uri(request, url)
-        # ret_bits = urlsplit(ret)
-        # custom_domain = get_domain_for_emails(request)
-        # return f"{ret_bits.scheme}://{custom_domain}{ret_bits.path}"
-        return ret
+    def get_email_confirmation_redirect_url(self, request):
+        if not config.frontend_host_domain:
+            return super().get_email_confirmation_redirect_url(request)
+
+        location = ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL
+        redirect_url =f"{request.scheme}://{config.frontend_host_domain}{location}"
+        logging.info(redirect_url)
+        return redirect_url
 
 
     def send_confirmation_mail(self, request, emailconfirmation, signup):
